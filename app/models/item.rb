d200f396904aee8869d7a095214b465ef2ddb661
ykkkkk0215 <168529522+ykkkkk0215@users.noreply.github.com>
2024-06-08 02:14:13 +0900

    商品詳細表示機能実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,6 +10,7 @@ class Item < ApplicationRecord
   has_one_attached :image
   belongs_to :user
   # has_one :order
+  
 
   validates :image,               presence: true
   validates :item_name,           presence: true
