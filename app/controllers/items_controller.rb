f01ab1672c8713774e4028e706d50a351dbd724d
ykkkkk0215 <168529522+ykkkkk0215@users.noreply.github.com>
2024-06-09 15:07:06 +0900

    4

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -34,7 +34,7 @@ class ItemsController < ApplicationController
     if @item.update(item_params)
       redirect_to @item, notice: 'Item was successfully updated.'
     else
-      render :edit
+      render :edit, status: :unprocessable_entity
     end
   end
 
